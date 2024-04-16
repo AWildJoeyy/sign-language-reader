159ab030ee01c720f8f16bc34f73696c60af7192
Joey <60157905+AWildJoeyy@users.noreply.github.com>
2024-04-16 12:01:59 -0400

    performance

--- training.py
+++ training.py
@@ -18,3 +18,7 @@ model = RandomForestClassifier()
 model.fit(x_train, y_train)
 
 y_predict = model.predict(x_test)
+
+score = accuracy_score(y_predict, y_test)
+
+print('{}% of samples were classified correctly'.format(score*100))
